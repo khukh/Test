62f7656eaf1ac679244a582ae1246aaa8e0f0d6d
Olga Khukh <olga.xuxrina@mail.ru>
2018-02-25 14:21:30 +0300

    Some adding were compleete blah blah blah

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        putchar(c);
+    }
+    puts("Hello, World!");
+    return 0;
+}
